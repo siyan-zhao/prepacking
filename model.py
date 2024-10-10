84de22266a248746cf74b73d97100db1d7420336
Daniel Israel <daniel.m.israel@gmail.com>
2024-10-10 10:30:01 -0700

    fix

--- model.py
+++ model.py
@@ -438,8 +438,6 @@ class CustomCausalLlamaModel(transformers.LlamaForCausalLM, GenerationMixin):
         >>> tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
         "Hey, are you conscious? Can you talk to me?\nI'm not conscious, but I can talk to you."
         ```"""
-        global flex_attention 
-        flex_attention = torch.compile(flex_attention)
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
